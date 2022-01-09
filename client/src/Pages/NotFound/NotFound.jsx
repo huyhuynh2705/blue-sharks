3bfb077592777e7389d3948681f3e36c2f1c40f9
Huynh Quang Huy <huy.huynh270520@gmail.com>
2022-01-09 21:18:25 +0700

    Add page not found

---
+++ client/src/Pages/NotFound/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div style={{ marginTop: '20%' }}>
+      <h1 style={{ textAlign: 'center' }}>Trang không tìm thấy</h1>
+      <a href="/">
+        <p style={{ textAlign: 'center' }}>Trở về trang chủ</p>
+      </a>
+    </div>
+  );
+};
+
+export default NotFound;
